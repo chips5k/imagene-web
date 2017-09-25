d25973185b596a28aef79c7dfbfcde720a827de1
Callem Pittard <callem.pittard@gmail.com>
2017-09-25 19:14:56 +1000

    Setup non decimal.js processing

--- public/sample-worker-decimal.js
+++ public/sample-worker-decimal.js
@@ -1,63 +1,82 @@
+importScripts('./decimal.min.js');
+
+Decimal.set({ precision: 5, rounding: 4 });
+
 /** The following are taken from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random */
 function getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min;
+    return Decimal.random().mul(max.sub(min)).add(min);
 }
 
-function getRandomInt (min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+getRandomInt = function(min, max) {
+    min = Decimal.ceil(min);
+    max = Decimal.floor(max);
+    return Decimal.floor(Decimal.random().mul(max.sub(min)).add(min)); //The maximum is exclusive and the minimum is inclusive
 }
+
 
 let operators = {
     double: {
         '+': (a, b) => { 
-            return a + b;
+            //console.log('+', a, b);
+            return a.add(b);
         },
         '-': (a, b) => { 
-            return a - b 
+            //console.log('-', a, b);
+            return a.sub(b) 
         },
         '/': (a, b) => { 
-            return a / b 
+            //console.log('/', a, b);
+            if(b == 0) {
+                return new Decimal(0);
+            }
+            return a.div(b) 
         },
         '*': (a, b) => { 
-            return a * b; 
+            //console.log('*', a, b);
+            return a.mul(b); 
         },
         '^': (a, b) => { 
-            return Math.pow(a, b); 
+            //console.log('^', a, b);
+            return Decimal.pow(a, b); 
         },
         '%': (a, b) => {
-            return a % b;
+            //console.log('%', a, b);
+            if(b == 0) {
+                return new Decimal(0);
+            }
+            return a.mod(b);
         },
         'CIR': (a, b) => {
-            return Math.sin(Math.sqrt(a * a + b * b) * Math.PI / 180.00);
+            //console.log('CIR', a, b);
+            return Decimal.sin(Decimal.sqrt(a.mul(a).add(b.mul(b)).mul(new Decimal(Math.PI)).div(180.00)));
         }
     },
     single: {
         'sqrt': (a) => {
-            return Math.sqrt(Math.abs(a));
+            return Decimal.sqrt(Decimal.abs(a));
         },
         'sin': (a) => {
-            return Math.sin(a % 3.16)
+            return Decimal.sin(a.mod(3.16));
         },
         'cos': (a) => {
-            return Math.cos(a % 3.16)
+            return Decimal.cos(a.mod(3.16))
         },
         'tan': (a) => {
-            return Math.tan(a)
+            return Decimal.tan(a)
         },
         'log': (a) => {
-            return Math.log(Math.abs(a));
+            return Decimal.log(Decimal.abs(a));
         }
     }
 };
 
 let operands = {
-    'pX': (x, y) => x,
-    'pY': (x, y) => y,
-    'PI': (x, y) => Math.PI,
-    'rand': (x, y) => getRandomArbitrary(0, 1000),
+    'pX': (x, y) => new Decimal(x),
+    'pY': (x, y) => new Decimal(y),
+    'PI': (x, y) => new Decimal(Math.PI),
+    'rand': (x, y) => getRandomArbitrary(new Decimal(0), new Decimal(1000)),
 }
+
 
 solveRpnExpression = function(expression, x, y) {
     let operandStack = [];
@@ -114,41 +133,43 @@ onmessage = function(e) {
         let g = solveRpnExpression(e.data.green.expression.slice(0), x, y);
         let b = solveRpnExpression(e.data.blue.expression.slice(0), x, y);
 
+
         array[i] = r;
         array[i + 1] = g;
         array[i + 2] = b;
         array[i + 3] = 255;
        
-        if(rgbRange.min.r === false || r < (rgbRange.min.r)) {
+        if(rgbRange.min.r === false || r.lessThan(rgbRange.min.r)) {
             rgbRange.min.r = r;
         }
 
-        if(rgbRange.min.g === false || g < (rgbRange.min.g)) {
+        if(rgbRange.min.g === false || g.lessThan(rgbRange.min.g)) {
             rgbRange.min.g = g;
         }
 
-        if(rgbRange.min.b === false || b < (rgbRange.min.b)) {
+        if(rgbRange.min.b === false || b.lessThan(rgbRange.min.b)) {
             rgbRange.min.b = b;
         }
 
-        if(rgbRange.max.r === false || r > (rgbRange.max.r)) {
+        if(rgbRange.max.r === false || r.greaterThan(rgbRange.max.r)) {
             rgbRange.max.r = r;
         }
 
-        if(rgbRange.max.g === false || g > (rgbRange.max.g)) {
+        if(rgbRange.max.g === false || g.greaterThan(rgbRange.max.g)) {
             rgbRange.max.g = g;
         }
 
-        if(rgbRange.max.b === false || b > (rgbRange.max.b)) {
+        if(rgbRange.max.b === false || b.greaterThan(rgbRange.max.b)) {
             rgbRange.max.b = b;
         }
+
     }
     
     
     let diffs = {
-        r: rgbRange.max.r - rgbRange.min.r,
-        g: rgbRange.max.g - rgbRange.min.g,
-        b: rgbRange.max.b - rgbRange.min.b,
+        r: rgbRange.max.r.sub(rgbRange.min.r),
+        g: rgbRange.max.g.sub(rgbRange.min.g),
+        b: rgbRange.max.b.sub(rgbRange.min.b),
     }
     
     let thresholdRanges = {
@@ -157,18 +178,23 @@ onmessage = function(e) {
         b: e.data.config.blueThresholdMax - e.data.config.blueThresholdMin
     }
 
+   
+
+
     for(let i = 0; i < e.data.image.data.length; i+=4) {
         
         let r = array[i];
         let g = array[i + 1];
         let b = array[i + 2];
         let a = array[i + 3];
-        
-        e.data.image.data[i] = ((r - rgbRange.min.r) * thresholdRanges.r / diffs.r) + e.data.config.redThresholdMin;
-        e.data.image.data[i + 1] = ((g - rgbRange.min.g) * thresholdRanges.g / diffs.g) + e.data.config.greenThresholdMin;
-        e.data.image.data[i + 2] = ((b - rgbRange.min.b) * thresholdRanges.b / diffs.b) + e.data.config.blueThresholdMin;
+
+        e.data.image.data[i] = parseInt(r.sub(rgbRange.min.r).mul(thresholdRanges.r).div(rgbRange.max.r - rgbRange.min.r).add(e.data.config.redThresholdMin));
+        e.data.image.data[i + 1] =  parseInt(g.sub(rgbRange.min.g).mul(thresholdRanges.g).div(rgbRange.max.g - rgbRange.min.g).add(e.data.config.greenThresholdMin));
+        e.data.image.data[i + 2] =  parseInt(b.sub(rgbRange.min.b).mul(thresholdRanges.b).div(rgbRange.max.b - rgbRange.min.b).add(e.data.config.blueThresholdMin));
         e.data.image.data[i + 3] = a;
     }   
+
+   console.log(e.data.image.data);
     
     postMessage(e.data.image);
 
