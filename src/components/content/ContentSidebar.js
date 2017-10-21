a2ecf7aa0f6a1853797eaf9ca1b665c5e003fc87
Callem Pittard <callem.pittard@gmail.com>
2017-10-21 14:17:53 +1100

    WIP Responsive styles

--- src/components/content/ContentSidebar.js
+++ src/components/content/ContentSidebar.js
@@ -3,6 +3,7 @@ import React from 'react';
 const ContentSidebar =(props) => {
     return (
         <div className="main__sidebar">
+            
             <a href="" onClick={props.toggleContentSidebar} className="main__content-sidebar-toggle">
                 <i className={`fa ${props.contentSidebarVisible ? 'fa-chevron-right' : 'fa-chevron-left'}`}></i>
             </a>
