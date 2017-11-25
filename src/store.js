702cd703dfc8c8105bac9c22f9dfdf1e1ab32f3f
Callem Pittard <callem.pittard@gmail.com>
2017-11-25 11:26:51 +1100

    Implement local storage

--- src/store.js
+++ src/store.js
@@ -1,14 +1,39 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { compose, createStore, combineReducers, applyMiddleware } from 'redux';
 import { routerReducer, routerMiddleware } from 'react-router-redux';
 import createHistory from 'history/createBrowserHistory';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
+
+import persistState from 'redux-localstorage'
 
 // Create a history of your choosing (we're using a browser history in this case)
 export const history = createHistory();
 
 // Build the middleware for intercepting and dispatching navigation actions
 const historyMiddleware = routerMiddleware(history);
+
+const slicer = (state) => {
+
+    const samples = {};
+
+    for(let id in state.samples.byId) {
+        let sample = state.samples.byId[id];
+
+        samples[id] = {
+            ...sample,
+            cache: {}
+        }
+    }
+
+    return {
+        ...state,
+        samples: {
+            ...state.samples,
+            byId: samples
+        }
+    };
+
+};  
 
 // Add the reducer to your store on the `router` key
 // Also apply our middleware for navigating
@@ -17,7 +42,10 @@ export const store = createStore(
       ...reducers,
       router: routerReducer
     }),
-    applyMiddleware(historyMiddleware, thunk)
+    compose(
+        applyMiddleware(historyMiddleware, thunk),
+        persistState(null, { slicer: (paths) => slicer })
+    )
 );
 
 export default store;
